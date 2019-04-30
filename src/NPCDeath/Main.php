db9aa0279268250589dffed7b5cd2e38717dc009
XxCreditIsGoodXx <50156391+XxCreditIsGoodXx@users.noreply.github.com>
2019-04-30 18:27:18 -0400

    Create Main.php

---
+++ src/NPCDeath/Main.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NPCDeath;
+
+use pocketmine\nbt\tag\{
+
+    CompoundTag, ListTag, DoubleTag, FloatTag
+};
+use pocketmine\entity\Entity;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable() : void{
+        Entity::registerEntity(DeathNPCEntity::class, true);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    /**
+     * @param PlayerDeathEvent $event
+     * @return void
+     */
+     
+    public function onDeath(PlayerDeathEvent $event) : void{
+        $player = $event->getPlayer();
+        $nbt = new CompoundTag("", [
+            new ListTag("Pos", [
+                new DoubleTag("", $player->getX()),
+                new DoubleTag("", $player->getY() - 1),
+                new DoubleTag("", $player->getZ())
+            ]),
+            new ListTag("Motion", [
+                new DoubleTag("", 0),
+                new DoubleTag("", 0),
+                new DoubleTag("", 0)
+            ]),
+            new ListTag("Rotation", [
+                new FloatTag("", 2),
+                new FloatTag("", 2)
+            ])
+        ]);
+        $nbt->setTag($player->namedtag->getTag("Skin"));
+        $npc = new DeathNPCEntity($player->getLevel(), $nbt);
+        $npc->getDataPropertyManager()->setBlockPos(DeathNPCEntity::DATA_PLAYER_BED_POSITION, new Vector3($player->getX(), $player->getY(), $player->getZ()));
+        $npc->setPlayerFlag(DeathNPCEntity::DATA_PLAYER_FLAG_SLEEP, true);
+        $npc->setNameTag("RIP " . $player->getName() . " died here");
+        $npc->setNameTagAlwaysVisible(false);
+        $npc->spawnToAll();
+        $this->getServer()->getScheduler()->scheduleDelayedTask(new DeathNPCClearTask($this, $npc, $player), 3600);
+    }
+    /**
+     * @param EntityDamageEvent $event
+     * @return void
+     */
+    public function onDamage(EntityDamageEvent $event) : void{
+        $entity = $event->getEntity();
+        if($entity instanceof DeathNPCEntity) $event->setCancelled(true);
+    }
+}
